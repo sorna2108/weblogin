2b84d3baecb0348d31a1de7d0fe22ebe4f25863b
sorna2108 <jagadeesansornalakshmi@gmail.com>
2022-11-07 18:55:47 +0530

    fifteenthcommit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,7 +1,7 @@
 const e = require("express");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Registration",{
+mongoose.connect("mongodb://Sorna:sorna2108@ac-fd0tjyx-shard-00-00.ahfarm2.mongodb.net:27017,ac-fd0tjyx-shard-00-01.ahfarm2.mongodb.net:27017,ac-fd0tjyx-shard-00-02.ahfarm2.mongodb.net:27017/?ssl=true&replicaSet=atlas-qd0b4j-shard-0&authSource=admin&retryWrites=true&w=majority",{
     
 }).then(()=>{
     console.log("connected successful");
